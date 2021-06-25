5d96ee3b35408709bae335bf6ba2bd04172567fc
aachalmakode <aachalmakode900@gmail.com>
2021-06-25 19:38:44 -0400

    100Hrs of 20days

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,36 +1,44 @@
-public class EmployeeWage{
+public class EmployeeWage {
 
-    public static final int isFullTime = 2;
+
     public static final int isPartTime = 1;
+    public static final int isFullTime = 2;
     public static final int empRatePerHour = 20;
     public static final int numOfWorkingDays = 20;
+    public static final int maxWorkingHours = 100;
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
 
         int empHours = 0;
-        int empWage  = 0;
+        int empWage = 0;
         int totalEmpWage = 0;
+        int totalWorkingHours = 0;
+        int totalWorkingDays = 0;
 
-        for(int day =0; day < numOfWorkingDays; day++) {
-            int empCheck = (int) Math.floor(Math.random()*10) % 3;
-            switch(empCheck){
-                case isFullTime:
-                    empHours = 8;
-                    break;
-                case isPartTime:
-                    empHours = 4;
-                    break;
-                default :
-                    empHours = 0;
+        while(totalWorkingHours <= maxWorkingHours &&
+                    totalWorkingDays <= numOfWorkingDays) {
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+                switch (empCheck) {
+                    case isFullTime:
+                        empHours = 8;
+                        break;
+                    case isPartTime:
+                        empHours = 4;
+                        break;
+                    default:
+                        empHours = 0;
+                }
+            empWage = empHours * empRatePerHour ;
+            totalEmpWage += empWage;
+            totalWorkingHours += empHours;
+            totalWorkingDays += 1;
             }
 
-            empWage = empHours * empRatePerHour;
-            totalEmpWage += empWage;
+
+            System.out.println("Total Wage is: " + totalEmpWage);
+
         }
-
-
-
-        System.out.println("Emp Wage for 20 working days: " + totalEmpWage);
     }
-}
+
